392292a66ba3cc0bf2e74ec3271a63be2950dca0
nuelchucks <mikkystormgmail.com>
2022-10-14 15:40:35 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
-int main(void) {printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
