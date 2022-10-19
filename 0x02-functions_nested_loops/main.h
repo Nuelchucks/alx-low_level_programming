ad290381aecaf258a400cb8b0e3f07b05f7e19fa
nuelchucks <mikkystormgmail.com>
2022-10-18 19:47:49 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN.H
 #define MAIN.H
+#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,5 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
